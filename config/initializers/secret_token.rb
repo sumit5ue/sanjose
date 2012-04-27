63f190afeda5dc2cd6df651ca1c165b2b1dc0de4
Sumit Agarwal <sumit5ue@yahoo.com>
2012-04-27 13:36:08 -0700

    empty project skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sanjose::Application.config.secret_token = '3a4ae04c7a567bf7b19205db319b8d77d303fc31296a6842a421584d2a770768e29dfa9a97715d343932b5a42d15c20eb77baeac098f321b481b1800e2692a1e'
